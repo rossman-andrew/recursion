51662d05faf53239cc318440cbb758fedaee7daa
Andrew Rossman <andrew100r@gmail.com>
2017-08-14 10:29:29 -0700

    Completed recursive implementation of stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,29 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if(obj === null || typeof obj === 'undefined') {
+  	return 'null';
+  } else if(typeof obj === 'string') {
+  	return '"' + obj + '"';
+  } else if(typeof obj === 'number') {
+  	return obj.toString();
+  } else if(typeof obj === 'boolean') {
+  	return obj.toString();
+  } else if(Array.isArray(obj)) {
+  	if(obj.length === 0){
+  		return '[]';
+  	}
+  	return '[' + obj.map(function(element) {
+  		return stringifyJSON(element);
+  	}).join(',') + ']';
+  } else if(typeof obj === 'object') {
+  	var objArray = [];
+  	for(var key in obj) {
+  		var value = stringifyJSON(obj[key]);
+  		if(key !== 'undefined' && key !== 'functions') {
+  			objArray.push('"' + key + '":' + value);	
+  		}
+  	}
+  	return "{" + objArray.join(',') + "}";
+  }
 };
